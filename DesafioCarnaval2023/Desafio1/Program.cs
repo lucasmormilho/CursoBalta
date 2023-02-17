dac5a0e17a43d07a1cbb911e2c4ee24d76ac7934
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-02-17 09:57:24 -0300

    desafio carnaval 1

---
+++ DesafioCarnaval2023/Desafio1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um texto:");
+
+var texto = Console.ReadLine();
+
+int qtdCaracteres = texto.Count();
+
+int qtdPalavras = texto.Split(" ").Count();
+
+if (texto.Count() == 0)
+    System.Console.WriteLine("Sem texto");
+else
+{
+    Console.WriteLine(qtdCaracteres + " caracteres, " + qtdPalavras + " palavras");
+}
+
+
