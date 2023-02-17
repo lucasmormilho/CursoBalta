caeee8e39567e24f92bac1719a39314233001c9c
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-02-17 09:33:14 -0300

    inicio do curso

---
+++ StopSwatch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            System.Console.WriteLine("S = Contar em segundos =>10s = 10 segundos");
+            System.Console.WriteLine("M = Contar em minutos => 1m = 1 minuto");
+            System.Console.WriteLine("0 = Sair");
+            System.Console.WriteLine("Quanto tempo deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
+
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                System.Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            System.Console.WriteLine("StopWatch Finaliado");
+            Thread.Sleep(2000);
+            Menu();
+        }
+    }
+}
