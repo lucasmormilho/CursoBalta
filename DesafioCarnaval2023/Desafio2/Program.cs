d16a98ab4a23e43edd83d75123ad87692c107457
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-02-19 08:51:07 -0300

    Desafio 2

---
+++ DesafioCarnaval2023/Desafio2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms\r\new-console-template for more information
+Console.Clear();
+Console.WriteLine("Vamos calcular seu IMC");
+System.Console.WriteLine("------------------------");
+System.Console.Write("Digite seu peso, separados por virgula:");
+double peso = double.Parse(Console.ReadLine());
+System.Console.Write("Agora, digite sua altura, separados por virgula:");
+double altura = double.Parse(Console.ReadLine());
+System.Console.WriteLine("------------------------");
+double imc = (peso / (altura * altura));
+System.Console.WriteLine($"Seu IMC é de {imc.ToString("F2")}");
+Imc(imc);
+
+static void Imc(double imc)
+{
+
+    if (imc < 18.5) System.Console.WriteLine("VOCÊ ESTÁ ABAIXO DO PESO");
+    if (imc >= 18.5 && imc <= 24.9) System.Console.WriteLine("VOCÊ ESTÁ COM PESO NORMAL");
+    if (imc >= 25 && imc <= 29.9) System.Console.WriteLine("VOCÊ ESTÁ COM PRÉ OBESIDADE");
+    if (imc >= 30 && imc <= 34.9) System.Console.WriteLine("VOCÊ ESTÁ COM OBESIDADE GRAU 1");
+    if (imc >= 35 && imc <= 39.9) System.Console.WriteLine("VOCÊ ESTÁ COM OBESIDADE GRAU 2");
+    if (imc > 40) System.Console.WriteLine("VOCÊ ESTÁ OBESIDADE GRAU 3");
+
+    System.Console.WriteLine("------------------------");
+    System.Console.WriteLine("fonte: OMS ( Organização Mundial da Saúde )");
+}
+
+
--- FundamentosCsharp/TextEditor/Program.cs
+++ FundamentosCsharp/TextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -6,8 +7,62 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-
+            Menu();
         }
 
+        static void Menu()
+        {
+            Console.Clear();
+            System.Console.WriteLine("O que você deseja fazer ?");
+            System.Console.WriteLine("1 - Abrir arquivo");
+            System.Console.WriteLine("2 - Criar novo arquivo");
+            System.Console.WriteLine("0 - Sair");
+
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+
+        }
+        static void Abrir()
+        {
+
+        }
+        static void Editar()
+        {
+            Console.Clear();
+            System.Console.WriteLine("Digite seu texto abaixo (ESC para sair):");
+            System.Console.WriteLine("---------------------");
+
+            string text = "";
+
+            //enquanto o ESC não for clicado
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.Write(text)
+        }
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            System.Console.WriteLine("Qual caminho para salvar o caminho?");
+            var path = Console.ReadLine();
+
+            //usar using para abrir e fechar
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+            System.Console.WriteLine("Salvo com sucesso!");
+            Menu();
+        }
     }
 }
