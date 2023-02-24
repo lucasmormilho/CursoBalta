85a64cce64c18143662d42db7aa1957a0aac652e
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-02-24 08:03:55 -0300

    classes partial, sealed, static

---
+++ OrientacaoObjeto/Payment2/Aulas/Partial_CreditCardPayment.cs
@@ -0,0 +1,9 @@
+namespace Payment2
+{
+    public partial class Payment
+    {
+        public int PropriedadeB { get; set; }
+        //é possivel dividir uma classe
+        ///faz mais sentido usar herança
+    }
+}
---
+++ OrientacaoObjeto/Payment2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Payment2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+    }
+}
+
+
