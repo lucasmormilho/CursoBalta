ee363b64d05d07cf539d0da6813dbd196c687c59
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-03-11 06:58:41 -0300

    inicio aula de acesso a dados usando connection string

---
+++ AcessoDados/BaltaDataAccess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BaltaDataAccess;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost,1433; DataBase=balta; Integrated Security=SSPI";
+        Console.WriteLine("Hello, World!");
+
+    }
+}
