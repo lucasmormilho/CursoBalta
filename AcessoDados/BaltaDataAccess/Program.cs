9b81d566a41e5961ecf1b8bcf0a735fa87fced95
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-03-11 08:12:25 -0300

    Conexao ADO e dapper

--- AcessoDados/BaltaDataAccess/Program.cs
+++ AcessoDados/BaltaDataAccess/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using BaltaDataAccess.Models;
 
 namespace BaltaDataAccess;
 
@@ -6,8 +9,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        const string connectionString = "Server=localhost,1433; DataBase=balta; Integrated Security=SSPI";
-        Console.WriteLine("Hello, World!");
+        const string connectionString = "Server= localhost\\SQLMORMILHO; Database= balta; Integrated Security=SSPI";
+
+        #region CONEXAO ADO
+        // usar using fechar a conexao automaticamente
+        using (var connection = new SqlConnection(connectionString))
+        {
+            System.Console.WriteLine("Conectado");
+            connection.Open();
+            using (var command = new SqlCommand())
+            {
+                //passar a conexao
+                command.Connection = connection;
+                //Definir qual  tipo de comando vai usar
+                command.CommandType = System.Data.CommandType.Text;
+                //Aplicar o comando
+                command.CommandText = "SELECT [id], [title] FROM [Category]";
+
+                //ler ou inserir as informações pelo dataREader
+                //podia ser executenonquery
+                var reader = command.ExecuteReader();
+
+                //percorrer
+                while (reader.Read())
+                {
+                    System.Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                }
+            }
+            Console.WriteLine("Fim");
+        }
+        #endregion
+
+        #region DAPPER
+
+        using (var connectionDapper = new SqlConnection(connectionString))
+        {
+            System.Console.WriteLine("Abre conexao dapper");
+            var categories = connectionDapper.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var Category in categories)
+            {
+                System.Console.WriteLine("Dapper" + " - " + Category.Id + " - " + Category.Title);
+            }
+            System.Console.WriteLine("fim dapper");
+        }
+
+        #endregion
 
     }
 }
